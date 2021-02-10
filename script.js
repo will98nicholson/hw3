ce1d6831908437eccea52b9dbba5f94a9ff6e362
William Nicholson <will98nicholson@gmail.com>
2021-02-10 14:37:05 -0500

    progress in the generator function

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90);
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122);
+const NUM_CHAR_CODES = arrayFromLowToHigh(48, 57);
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)).concat(arrayFromLowToHigh(91, 96)).concat(arrayFromLowToHigh(123, 126));
 
 // Write password to the #password input
 function writePassword() {
@@ -10,18 +13,41 @@ function writePassword() {
   passwordText.value = password;
 
 }
-var generatePassword = function () {
-  var passLength = window.prompt("choose password length");
+function generatePassword() {
+
+  passLength = window.prompt("choose password length");
   if (passLength < 8) {
     return "password must be at least 8 characters of length"
   };
   if (passLength > 128) {
     return "password cannot be longer than 128 characters in length"
   };
+  var includeCapital = window.confirm("include capital letters?");
+
+  var includeNumber = window.confirm("include numbers?");
+
+  var includeSymbol = window.confirm("include symbols?");
+
+  let charCodes = LOWERCASE_CHAR_CODES
+
+  if (includeCapital === true) { charCodes = charCodes.concat(UPPERCASE_CHAR_CODES); }
+  if (includeNumber === true) { charCodes = charCodes.concat(NUM_CHAR_CODES); }
+  if (includeSymbol === true) { charCodes = charCodes.concat(SYMBOL_CHAR_CODES); }
+  const passwordCharacters = [];
+  for (let i = 0; i < passLength.value, i++;) {
+    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)];
+    passwordCharacters.push(String.fromCharCode(characterCode));
+  } return passwordCharacters.join('');
+
 }
 
-
-
+function arrayFromLowToHigh(low, high) {
+  const array = [];
+  for (let i = low; i <= high; i++) {
+    array.push(i);
+  }
+  return array;
+}
 
 
 
