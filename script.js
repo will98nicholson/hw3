71ca289b5be1a329d3d5c485be11dddb47ce00a1
William Nicholson <will98nicholson@gmail.com>
2021-02-10 18:12:08 -0500

    bug fixes, should have working generator now

--- script.js
+++ script.js
@@ -4,13 +4,19 @@ const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90);
 const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122);
 const NUM_CHAR_CODES = arrayFromLowToHigh(48, 57);
 const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)).concat(arrayFromLowToHigh(91, 96)).concat(arrayFromLowToHigh(123, 126));
-
+function arrayFromLowToHigh(low, high) {
+  const array = [];
+  for (let i = low; i <= high; i++) {
+    array.push(i);
+  }
+  return array;
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = password
 
 }
 function generatePassword() {
@@ -19,32 +25,27 @@ function generatePassword() {
   if (passLength < 8 || passLength > 128) {
     return "password must be at least 8 characters and no longer than 128 characters in length"
   };
+
   var includeCapital = window.confirm("include capital letters?");
 
   var includeNumber = window.confirm("include numbers?");
 
   var includeSymbol = window.confirm("include symbols?");
+  var charCodes = LOWERCASE_CHAR_CODES
 
-  let charCodes = LOWERCASE_CHAR_CODES
+  if (includeCapital === true) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES);
+  if (includeNumber === true) charCodes = charCodes.concat(NUM_CHAR_CODES);
+  if (includeSymbol === true) charCodes = charCodes.concat(SYMBOL_CHAR_CODES);
 
-  if (includeCapital === true) { charCodes = charCodes.concat(UPPERCASE_CHAR_CODES); }
-  if (includeNumber === true) { charCodes = charCodes.concat(NUM_CHAR_CODES); }
-  if (includeSymbol === true) { charCodes = charCodes.concat(SYMBOL_CHAR_CODES); }
-  const passwordCharacters = [];
-  for (let i = 0; i < passLength.value; i++) {
+  const chosenChars = [];
+  for (let i = 0; i < passLength; i++) {
     const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)];
-    passwordCharacters.push(String.fromCharCode(characterCode));
-  } return passwordCharacters.join('');
+    chosenChars.push(String.fromCharCode(characterCode));
+  } return chosenChars.join('');
+
 
 }
 
-function arrayFromLowToHigh(low, high) {
-  const array = [];
-  for (let i = low; i <= high; i++) {
-    array.push(i);
-  }
-  return array;
-}
 
 
 
